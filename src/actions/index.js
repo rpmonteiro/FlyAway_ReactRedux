e2491c95152a7fe7cd9c9a953c740fdb7be72a2c
rpmonteiro <ric.p.monteiro@gmail.com>
2016-04-27 20:20:24 +0100

    fixed the return value of the grabURLS

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,5 @@
 import { FETCH_FLICKR_IMG } from './types';
 import axios from 'axios';
-import filter from 'lodash/filter';
 
 const API_KEY = 'b6b35718f3c50d1c2c6d04ead7f016b2';
 const ROOT_URL = ` https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${API_KEY}&privacy_filter=1&safe_search=1&group_id=41425956%40N00&per_page=30&page=1&format=json&nojsoncallback=1&sort=interestingness-desc`;
@@ -12,23 +11,22 @@ function grabUrls(photos) {
     const farmId = photo.farm;
     const serverId = photo.server;
     const secret = photo.secret;
-    // "https://farm" + farmId + ".staticflickr.com/" + serverId + "/" + id + "_" + secret + "_h.jpg"
     urls.push(`https://farm${farmId}.staticflickr.com/${serverId}/${id}_${secret}_b.jpg`);
   }
-  console.log(urls);
+  return urls;
 }
 
 export function fetchFlickrImages(location) {
   const url = `${ROOT_URL}&tags=${location}`;
-  const request = axios.get(url);
+  let result;
 
   axios.get(url)
     .then((response) => {
-      grabUrls(response.data.photos.photo);
+      result = grabUrls(response.data.photos.photo);
     });
 
   return {
     type: FETCH_FLICKR_IMG,
-    payload: request,
+    payload: result,
   };
 }
